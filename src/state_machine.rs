cd40c8fe7c72697241b832b14390501a2ebd06ec
冬冬 李 <lsongzhi@163.com>
2019-04-17 16:56:16 +0800

    edge

--- src/state_machine.rs
+++ src/state_machine.rs
@@ -64,12 +64,12 @@ enum StateMachineWrapper {
     DecimalExponentSignedAcc(StateMachine<DecimalExponentSignedAcc>),
     Identifier(StateMachine<Identifier>),
     SlashAcc(StateMachine<SlashAcc>),
-    LCurlyAcc(StateMachine<LBraceAcc>),
-    RCurlyAcc(StateMachine<RBraceAcc>),
-    LRoundAcc(StateMachine<LParenAcc>),
-    RRoundAcc(StateMachine<RParenAcc>),
-    LSquareAcc(StateMachine<LBracketAcc>),
-    RSquareAcc(StateMachine<RBracketAcc>),
+    LBraceAcc(StateMachine<LBraceAcc>),
+    RBraceAcc(StateMachine<RBraceAcc>),
+    LParenAcc(StateMachine<LParenAcc>),
+    RParenAcc(StateMachine<RParenAcc>),
+    LBracketAcc(StateMachine<LBracketAcc>),
+    RBracketAcc(StateMachine<RBracketAcc>),
 
     InputElementDiv(StateMachine<InputElementDiv>),
     Slash(StateMachine<Slash>),
@@ -103,3 +103,83 @@ enum StateMachineWrapper {
     DecimalExponent(StateMachine<DecimalExponent>),
     DecimalExponentSigned(StateMachine<DecimalExponentSigned>),
 }
+
+Edge!(InputElementDiv, LineTerminator);
+Edge!(InputElementDiv, WhiteSpace);
+Edge!(InputElementDiv, Slash);
+Edge!(InputElementDiv, DotPart);
+Edge!(Slash, SlashAcc);
+Edge!(Slash, SingleLineComment);
+Edge!(Slash, MultiLineComment);
+Edge!(SingleLineComment, SingleLineCommentAcc);
+Edge!(MultiLineComment, MultiLineCommentStar);
+Edge!(MultiLineCommentStar, MultiLineCommentAcc);
+Edge!(InputElementDiv, Identifier);
+Edge!(InputElementDiv, LBrace);
+Edge!(LBrace, LBraceAcc);
+Edge!(InputElementDiv, RBrace);
+Edge!(RBrace, RBraceAcc);
+Edge!(InputElementDiv, LParen);
+Edge!(LParen, LParenAcc);
+Edge!(InputElementDiv, RParen);
+Edge!(RParen, RParenAcc);
+Edge!(InputElementDiv, LBracket);
+Edge!(LBracket, LBracketAcc);
+Edge!(InputElementDiv, RBracket);
+Edge!(RBracket, RBracketAcc);
+Edge!(InputElementDiv, Semicolon);
+Edge!(Semicolon, SemicolonAcc);
+Edge!(InputElementDiv, Comma);
+Edge!(Comma, CommaAcc);
+Edge!(InputElementDiv, Colon);
+Edge!(Colon, ColonAcc);
+Edge!(InputElementDiv, QuestionMark);
+Edge!(QuestionMark, QuestionMarkAcc);
+Edge!(InputElementDiv, Tilde);
+Edge!(Tilde, TildeAcc);
+Edge!(InputElementDiv, Lt);
+Edge!(Lt, LtAcc);
+Edge!(InputElementDiv, Gt);
+Edge!(Gt, GtAcc);
+Edge!(InputElementDiv, Assign);
+Edge!(Assign, AssignAcc);
+Edge!(InputElementDiv, Exclamation);
+Edge!(Exclamation, ExclamationAcc);
+Edge!(InputElementDiv, Plus);
+Edge!(Plus, PlusAcc);
+Edge!(InputElementDiv, Multi);
+Edge!(Multi, MultiAcc);
+Edge!(InputElementDiv, Mod);
+Edge!(Mod, ModAcc);
+Edge!(InputElementDiv, Minus);
+Edge!(Minus, MinusAcc);
+Edge!(InputElementDiv, Or);
+Edge!(Or, OrAcc);
+Edge!(InputElementDiv, ExclusiveOr);
+Edge!(ExclusiveOr, ExclusiveOrAcc);
+Edge!(InputElementDiv, String);
+Edge!(And, AndAcc);
+Edge!(InputElementDiv, And);
+Edge!(InputElementDiv, SawZero);
+Edge!(SawZero, Decimal);
+Edge!(SawZero, DecimalAcc);
+Edge!(InputElementDiv, Decimal);
+Edge!(Decimal, DecimalAcc);
+Edge!(Decimal, DecimalExponent);
+Edge!(DecimalDigits, DecimalExponent);
+Edge!(DecimalDigits, DecimalDigitsAcc);
+Edge!(DecimalExponent, DecimalExponentSigned);
+Edge!(DecimalExponentSigned, DecimalExponentSignedAcc);
+Edge!(DecimalExponent, DecimalExponentAcc);
+Edge!(Decimal, DecimalDigits);
+Edge!(InputElementDiv, Hex);
+Edge!(Hex, HexAcc);
+Edge!(SawZero, Hex);
+Edge!(InputElementDiv, Binary);
+Edge!(Binary, BinaryAcc);
+Edge!(SawZero, Binary);
+Edge!(InputElementDiv, Octal);
+Edge!(Octal, OctalAcc);
+Edge!(SawZero, Octal);
+Edge!(MultiLineCommentStar, MultiLineComment);
+Edge!(LineTerminator, HELL);
