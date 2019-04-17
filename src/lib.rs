621f13fd052225116217a4e5832943ee6cd7b501
冬冬 李 <lsongzhi@163.com>
2019-04-17 21:40:25 +0800

    Lexer

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,5 @@
 #![feature(proc_macro_hygiene)]
 
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
-#![allow(unused_macros)]
-
 #[macro_use]
 extern crate phf;
 
@@ -18,8 +13,25 @@ mod state;
 mod equivalence;
 mod state_machine;
 pub mod error;
+
 /// Module for efficient string representation
 pub mod internship {
     extern crate internship;
+
     pub use internship::*;
+}
+
+use self::{state_machine::parse, token::*};
+
+/// Lexer implementation
+#[derive(Debug, Copy, Clone)]
+pub struct Lexer;
+
+impl Lexer {
+    /// Transform string to stream of tokens
+    pub fn lex(s: &str) -> Result<Vec<Token>, error::Error> {
+        let mut tokens = parse(s)?;
+        tokens.push(Token::EOF);
+        Ok(tokens)
+    }
 }
