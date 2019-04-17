3d4a1b56f3fd99b696420cc0e02cdd91c696da69
冬冬 李 <lsongzhi@163.com>
2019-04-17 15:13:45 +0800

    parse_identifier

--- src/identifier.rs
+++ src/identifier.rs
@@ -1,4 +1,5 @@
 use crate::token::Token;
+use internship::IStr;
 use std::str;
 
 const KEYWORDS: phf::Map<&'static str, Token> = phf_map! {
@@ -48,3 +49,32 @@ const KEYWORDS: phf::Map<&'static str, Token> = phf_map! {
     "_Thread_local" => Token::ThreadLocal,
     "__func__" => Token::FuncName,
 };
+
+#[inline]
+fn is_identifier_part(cp: u8) -> bool {
+    cp == 0x24
+        || cp == 0x5F
+        || (cp >= 0x41 && cp <= 0x5A)
+        || (cp >= 0x61 && cp <= 0x7A)
+        || (cp >= 0x30 && cp <= 0x39)
+        || cp == 0x5C
+        || cp >= 0x80
+}
+
+#[inline]
+pub fn parse_identifier(input: &[u8], c_src: &mut usize) -> Token {
+    let mut it = 0;
+    for i in 0..input.len() - *c_src {
+        if !unsafe { is_identifier_part(*input.get_unchecked(*c_src + i)) } {
+            it = i;
+            break;
+        }
+    }
+    let ident = &input[*c_src - 1..*c_src + it];
+    *c_src += it;
+    let ident = unsafe { str::from_utf8_unchecked(ident) };
+    KEYWORDS
+        .get(ident)
+        .cloned()
+        .unwrap_or_else(|| Token::Identifier(IStr::new(ident)))
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
 #![feature(proc_macro_hygiene)]
 
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(unused_macros)]
+
 #[macro_use]
 extern crate phf;
 
